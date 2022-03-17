dcdbd61b973b661f9be6cb8bfd54b18bdabc6152
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2022-03-17 17:26:27 +0700

    Update app.py

--- src/taipy/rest/app.py
+++ src/taipy/rest/app.py
@@ -25,8 +25,7 @@ def create_app(testing=False):
         abs_folder, _ = TAIPY_SETUP_FILE.rsplit("/", 1)
         gui = Gui(flask=app, pages={"demo": Markdown(os.path.join(abs_folder, "demo.md"))})
         gui.run(run_server=False)
-    else:
-        pass
+
     return app
 
 
