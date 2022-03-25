d8f5402f9df2bddf0f59bbff39316a515ed0910b
Vuillemot Florian <florian.vuillemot@posteo.net>
2022-03-25 10:40:11 +0100

    Update argument namming

--- src/taipy/rest/run.py
+++ src/taipy/rest/run.py
@@ -1,6 +1,6 @@
 from .app import create_app
 
 
-def run(*args, **kwargs):
+def run(testing=False, flask_env=None, secret_key=None, *args, **kwargs):
     app = create_app(*args, **kwargs)
     return app.run(*args, **kwargs)
