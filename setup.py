2b699f52b4f5dbe4e3d0e0ee053ba428a2bb2309
Vuillemot Florian <florian.vuillemot@posteo.net>
2022-03-25 08:45:34 +0100

    Fix package inclusion

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup, find_namespace_packages, find_packages
 
 __version__ = "1.0.0.dev"
 
-extras = {
-    "gui": ["taipy-gui@git+ssh://git@github.com/Avaiga/taipy-gui.git@develop"],
-}
 
 setup(
     name="taipy_rest",
     version=__version__,
-    packages=find_namespace_packages(where='src'),
+    packages=find_namespace_packages(where='src') + find_packages(include=["taipy.rest"]),
     package_dir={'': 'src'},
     install_requires=[
         "flask",
@@ -25,5 +22,4 @@ setup(
         "apispec-webframeworks",
         "taipy-core@git+ssh://git@github.com/Avaiga/taipy-core.git@develop"
     ],
-    extras_require=extras,
 )
